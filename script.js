1db720eba2952e5f03e775492addfa3c91743249
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-21 11:00:07 -0300

    Adicionando funcionalidade

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnShowModal = document.querySelectorAll('.show-modal')
+
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', openModal)
+}
+
+overlay.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal)
